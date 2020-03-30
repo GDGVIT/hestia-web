98b251508ae1a0b1c94a3f438963c49727651b70
Ashutosh Kaushik <ash@pop-os.localdomain>
2020-03-31 01:00:59 +0530

    fix

--- src/components/profile/suggestions.js
+++ src/components/profile/suggestions.js
@@ -1,1 +1,18 @@
 import React from 'react'
+
+class Suggestions extends React.Component{
+    constructor(props){
+        super(props);
+        this.state={
+
+        }
+    }
+    render(){
+        return(
+            <div>
+
+            </div>
+        )
+    }
+}
+export default Suggestions;
