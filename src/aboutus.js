386e01e1b43839108fb7efb40553a172e31e5831
Ashutosh Kaushik <ash@pop-os.localdomain>
2020-04-05 19:16:46 +0530

    borders

--- src/aboutus.js
+++ src/aboutus.js
@@ -36,7 +36,7 @@ const Aboutus = (props) =>{
             <h3>FEED</h3>
         </div>
     </div>
-    <div className="border"></div>
+    {/* <div className="border"></div> */}
     <div className="content">
         <h3>The go-to place for everyone that needs help with supplies and wants to help out others in need of supplies. This section displays a continuous list of resources people around you are in need of as well as an option for you to add your requests. For the listed requests, you can either suggest a shop that can supply the resource or help them out yourself if you have some to spare. When a request is accepted, Akina provides a chat room between the requestor, the person who made the request, and the requestee, the person willing to help, to coordinate and facilitate communication.
         </h3>
@@ -52,7 +52,7 @@ const Aboutus = (props) =>{
                 <h3>NEWS</h3>
             </div>
         </div>
-        <div className="border2"></div>
+        {/* <div className="border2"></div> */}
         <div className="content">
             <h3 className="content1">One of the most important issue, to tackle during this time of crisis, is to stay informed but various sources spreading misinformation makes it very difficult to decide which resource is legitimate. Hence, we only bring you official news from The World Health Organisation to make sure each update we give you is credible.</h3>
         </div>
@@ -74,7 +74,7 @@ const Aboutus = (props) =>{
                     <h3>EXPLORE</h3>
                 </div>
             </div>
-        <div className="border3">&nbsp;</div>
+        {/* <div className="border3">&nbsp;</div> */}
         <div className="content">
             <h3>In times of need, trying to find resources is not only difficult but stressful as well. The explore section brings you a list of resources, active organizations around you to help you get what you need with the least effort right when you need them.
             </h3>
@@ -90,7 +90,7 @@ const Aboutus = (props) =>{
                     <h3>UPDATES</h3>
                 </div>
             </div>
-            <div className="border2">&nbsp;</div>
+            {/* <div className="border2">&nbsp;</div> */}
             <div className="content">
                 <h3 className="content1">We are actively finding sources, apart from W.H.O., for credible information and integrating them into the platform to provide information relevant not just to the global scope but to you as an individual.</h3>
             </div>
