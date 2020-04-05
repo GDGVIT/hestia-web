5a3c41f359ed32065f558c28b36060253c5842a4
Ananya <ananyaganesh01@gmail.com>
2020-04-05 18:45:47 +0530

    Added link to app and web about us changes

--- src/desktop/aboutus.js
+++ src/desktop/aboutus.js
@@ -3,6 +3,7 @@ import logo from '../assets/logo.png';
 import explore from '../assets/explore.png';
 import feed from '../assets/feed.png';
 import news from '../assets/news2.png';
+import Akina from '../assets/akina.apk';
 
 
 const Aboutus = (props) =>{
@@ -19,9 +20,11 @@ const Aboutus = (props) =>{
     <div className="buttons">
                 <div className="button1">
                     <button onClick={()=>{props.history.push("/dlogin")}}>Visit Website</button>
+                    <p style={{marginTop:"10px",cursor:"pointer",color:"#989898"}} onClick={()=>{props.history.push("/dlogin")}}>PWA available on the web app.</p>
                 </div>
                 <div className="button2">
-                    <button>Download App</button>
+                    <a className="download" href={Akina} download="Akina">Download App</a>
+                    <p className="covid">Due to covid 19 our app is taking some time to process on the Play Store, meanwhile you can download it from our server.</p>
                 </div>
             </div>
     <div className="features">
@@ -29,7 +32,7 @@ const Aboutus = (props) =>{
     </div>
     <div className="parent1">
     <div className="feed">
-    <div className="feedlogo" style={{marginTop:"10px"}}>
+    <div className="feedlogo">
         <div className="circle">
             <img src={feed}></img>
         </div>
@@ -37,7 +40,7 @@ const Aboutus = (props) =>{
             <h3>Feed</h3>
         </div>
     </div>
-    <div className="border"></div>
+    {/* <div className="border"></div> */}
     <div className="content">
         <h3>The go-to place for everyone that needs help with supplies and wants to help out others in need of supplies. This section displays a continuous list of resources people around you are in need of as well as an option for you to add your requests. For the listed requests, you can either suggest a shop that can supply the resource or help them out yourself if you have some to spare. When a request is accepted, Akina provides a chat room between the requestor, the person who made the request, and the requestee, the person willing to help, to coordinate and facilitate communication.
         </h3>
@@ -53,7 +56,7 @@ const Aboutus = (props) =>{
                 <h3>News</h3>
             </div>
         </div>
-        <div className="border2"></div>
+        {/* <div className="border2"></div> */}
         <div className="content">
             <h3 className="content1">One of the most important issue, to tackle during this time of crisis, is to stay informed but various sources spreading misinformation makes it very difficult to decide which resource is legitimate. Hence, we only bring you official news from The World Health Organisation to make sure each update we give you is credible.</h3>
         </div>
@@ -75,7 +78,7 @@ const Aboutus = (props) =>{
                     <h3>Explore</h3>
                 </div>
             </div>
-        <div className="border3">&nbsp;</div>
+        {/* <div className="border3">&nbsp;</div> */}
         <div className="content">
             <h3>In times of need, trying to find resources is not only difficult but stressful as well. The explore section brings you a list of resources, active organizations around you to help you get what you need with the least effort right when you need them.
             </h3>
@@ -91,7 +94,7 @@ const Aboutus = (props) =>{
                     <h3>Updates</h3>
                 </div>
             </div>
-            <div className="border2">&nbsp;</div>
+            {/* <div className="border2">&nbsp;</div> */}
             <div className="content">
                 <h3 className="content1">We are actively finding sources, apart from W.H.O., for credible information and integrating them into the platform to provide information relevant not just to the global scope but to you as an individual.</h3>
             </div>
@@ -102,9 +105,11 @@ const Aboutus = (props) =>{
             <div className="buttons">
                 <div className="button1">
                     <button onClick={()=>{props.history.push("/dlogin")}}>Visit Website</button>
+                    <p style={{marginTop:"10px",cursor:"pointer",color:"#989898"}} onClick={()=>{props.history.push("/dlogin")}}>PWA available on the web app.</p>
                 </div>
                 <div className="button2">
-                    <button>Download App</button>
+                    <a className="download" href={Akina} download="Akina">Download App</a>
+                    <p className="covid">Due to covid 19 our app is taking some time to process on the Play Store, meanwhile you can download it from our server.</p>
                 </div>
             </div>
     
