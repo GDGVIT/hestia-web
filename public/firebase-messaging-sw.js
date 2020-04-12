33fb4b7fe5ba8c047b48561e398b9ed493ef7817
Ananya <ananyaganesh01@gmail.com>
2020-04-12 16:51:28 +0530

    Fix import script error

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,6 +1,6 @@
 importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-messaging.js');
-importScripts('https://www.gstatic.com/firebasejs/init.js');
+// importScripts('https://www.gstatic.com/firebasejs/init.js');
 
 
 firebase.initializeApp({
